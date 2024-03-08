feb139e63b2971898e882691594d2b61f5680010
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-03-07 23:33:04 -0300

    Update and rename createtable.sql to main.sql

--- projetos/sistema_livraria/sql/main.sql
+++ projetos/sistema_livraria/sql/main.sql
@@ -1,3 +1,4 @@
+-- CRIAÇÃO DAS TABELAS
 CREATE TABLE Usuario(
 	id INT PRIMARY KEY NOT NULL,
 	nome VARCHAR(30) NOT NULL,
@@ -6,23 +7,22 @@ CREATE TABLE Usuario(
 	senha VARCHAR(20) NOT NULL,
 	telefone VARCHAR(30),
 	endereco VARCHAR(150),
-    CHECK ((tipo = 'cliente' AND endereco IS NOT NULL) OR (tipo = 'funcionário' AND endereco IS NULL))
+	CHECK ((tipo = 'cliente' AND endereco IS NOT NULL) OR (tipo = 'funcionário' AND endereco IS NULL))
 );
 
 CREATE TABLE Livro(
 	id INT PRIMARY KEY NOT NULL,
-    titulo VARCHAR(50) NOT NULL,
-    autor VARCHAR(60) NOT NULL,
-    editora VARCHAR(30) NOT NULL,
-    paginas INT NOT NULL,
-    volume INT NOT NULL,
-    idioma VARCHAR(30) NOT NULL,
-    ano_publicacao INT NOT NULL,
-    condicao VARCHAR(10) NOT NULL CHECK (condicao IN ('novo', 'usado')),
-    qtd_disponivel INT NOT NULL,
-    qtd_reservado INT NOT NULL,
-    valor_unitario FLOAT NOT NULL,
-    pontos_troca INT NOT NULL
+	titulo VARCHAR(50) NOT NULL,
+	autor VARCHAR(60) NOT NULL,
+	paginas INT NOT NULL,
+	volume INT NOT NULL,
+	idioma VARCHAR(30) NOT NULL,
+	ano_publicacao INT NOT NULL,
+	condicao VARCHAR(10) NOT NULL CHECK (condicao IN ('novo', 'usado')),
+	qtd_disponivel INT NOT NULL,
+	qtd_reservado INT NOT NULL,
+	valor_unitario FLOAT NOT NULL,
+	pontos_troca INT NOT NULL
 );
 
 
@@ -30,16 +30,16 @@ CREATE TABLE Pedido(
 	id INT PRIMARY KEY NOT NULL,
 	id_usuario INT NOT NULL,
 	tipo VARCHAR(20) CHECK (tipo IN ('Compra', 'Empréstimo')),
-    status VARCHAR(30) CHECK (status IN ('Aprovado', 'Cancelado', 'Aguardando', 
-    	'Em reembolso', 'Reembolsado', 'Emprestado', 'Devolvido')),
-    motivo_reemb varchar(60),
-    data_ped DATE,
-    forma_pagamento VARCHAR(30) CHECK (forma_pagamento IN ('Cartão', 'Pix', 'Boleto', 'Permuta')),
-    data_emp DATE,
-    data_fim DATE,
-    valor_total FLOAT,
-    data_reemb date,
-    FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
+	status VARCHAR(30) CHECK (status IN ('Aprovado', 'Cancelado', 'Aguardando',
+		'Em reembolso', 'Reembolsado', 'Emprestado', 'Devolvido')),
+	motivo_reemb varchar(60),
+	data_ped DATE,
+	forma_pagamento VARCHAR(30) CHECK (forma_pagamento IN ('Cartão', 'Pix', 'Boleto', 'Permuta')),
+	data_emp DATE,
+	data_fim DATE,
+	valor_total FLOAT,
+	data_reemb date,
+	FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
 );
 
 
@@ -54,20 +54,21 @@ CREATE TABLE Carrinho(
 	FOREIGN KEY (id_livro) REFERENCES Livro(id)
 );
 
+-- INSERÇÃO DOS USUÁRIOS
 insert into Usuario 
-    values (1, 'Eric Castro', 'cliente', 'ericc14@gmail.com', '12345678', '(11) 95555-9999', 'Rua dos Guararapes, 745, São Paulo-SP');
+	values (1, 'Eric Castro', 'cliente', 'ericc14@gmail.com', '12345678', '(11) 95555-9999', 'Rua dos Guararapes, 745, São Paulo-SP');
 insert into Usuario (id, nome, tipo, email, senha, telefone)
-    values (2, 'Eric Castro', 'funcionário', 'ericc14@gmail.com', '12345678', '(11) 95555-9999');
+	values (2, 'Eric Castro', 'funcionário', 'ericc14@gmail.com', '12345678', '(11) 95555-9999');
 insert into Usuario 
-    values (3, 'Gabriel', 'cliente', 'Gabriel2000@gmail.com', 'ABCD123', '(11) 95555-1001', 'Estrada de taipas, 1990, São Paulo-SP');
+	values (3, 'Gabriel', 'cliente', 'Gabriel2000@gmail.com', 'ABCD123', '(11) 95555-1001', 'Estrada de taipas, 1990, São Paulo-SP');
 insert into Usuario 
-    values (4, 'Geovanna Vieira', 'cliente', 'Geovieira059@gmail.com', 'ABCD123', '(11) 95555-1002', 'Estrada de taipas, 1990, São Paulo-SP');
+	values (4, 'Geovanna Vieira', 'cliente', 'Geovieira059@gmail.com', 'ABCD123', '(11) 95555-1002', 'Estrada de taipas, 1990, São Paulo-SP');
 insert into Usuario 
-    values (5, 'Rosangela Santos', 'cliente', 'Rosangela.santos@gmail.com', '0246810', '(11) 95555-1030', 'Estrada de taipas, 1990, São Paulo-SP');
+	values (5, 'Rosangela Santos', 'cliente', 'Rosangela.santos@gmail.com', '0246810', '(11) 95555-1030', 'Estrada de taipas, 1990, São Paulo-SP');
 select * from Usuario;
 
 
--- INSERÇÕES DOS LIVROS
+-- INSERÇÃO DOS LIVROS
 insert into Livro values (1, 'Memórias Póstumas', 'Machado De Assis', 'Antofágica', 200, 2, 'PT/BR', 2007, 'novo', 8, 2, 20, 2);
 insert into Livro values (2, 'Mentirosos', 'Lockhart', 'Seguinte', 420, 1, 'PT/BR', 2016, 'novo', 7, 7, 50, 4);
 insert into Livro values (3, 'Memórias Póstumas', 'Machado De Assis', 'Antofágica', 200, 2, 'PT/BR', 2007, 'usado', 8, 2, 20, 2);
