6200fcccdb69e3a37d2d8755eb1dfe6406d97a2b
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-07-30 23:13:58 -0300

    Update main.sql

--- projetos/sistema_livraria/sql/main.sql
+++ projetos/sistema_livraria/sql/main.sql
@@ -1,4 +1,6 @@
--- CRIAÇÃO DAS TABELAS
+----- Criação das Tabelas -----
+
+-- Tabela para usuários
 CREATE TABLE Usuario (
 	id INT PRIMARY KEY NOT NULL,
 	nome VARCHAR(30) NOT NULL,
@@ -10,6 +12,7 @@ CREATE TABLE Usuario (
 	CHECK ((tipo = 'cliente' AND endereco IS NOT NULL) OR (tipo = 'funcionário' AND endereco IS NULL))
 );
 
+-- Tabela para livros
 CREATE TABLE Livro (
 	id INT PRIMARY KEY NOT NULL,
 	titulo VARCHAR(50) NOT NULL,
@@ -25,13 +28,13 @@ CREATE TABLE Livro (
 	pontos_troca INT NOT NULL
 );
 
-
+-- Tabela para pedidos
 CREATE TABLE Pedido (
 	id INT PRIMARY KEY NOT NULL,
 	id_usuario INT NOT NULL,
 	tipo VARCHAR(20) CHECK (tipo IN ('Compra', 'Empréstimo')),
 	status VARCHAR(30) CHECK (status IN ('Aprovado', 'Cancelado', 'Aguardando', 'Em reembolso', 'Reembolsado', 'Emprestado', 'Devolvido')),
-	motivo_reemb varchar(60),
+	motivo_reemb VARCHAR(60),
 	data_ped DATE,
 	forma_pagamento VARCHAR(30) CHECK (forma_pagamento IN ('Cartão', 'Pix', 'Boleto', 'Permuta')),
 	data_emp DATE,
@@ -41,7 +44,7 @@ CREATE TABLE Pedido (
 	FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
 );
 
-
+-- Tabela para carrinho de compras
 CREATE TABLE Carrinho (
 	id_pedido INT NOT NULL,
 	id_livro INT NOT NULL,
@@ -52,27 +55,3 @@ CREATE TABLE Carrinho (
 	FOREIGN KEY (id_pedido) REFERENCES Pedido(id),
 	FOREIGN KEY (id_livro) REFERENCES Livro(id)
 );
-
--- INSERÇÃO DOS USUÁRIOS
-INSERT INTO Usuario 
-	VALUES (1, 'Eric Castro', 'cliente', 'ericc14@gmail.com', '12345678', '(11) 95555-9999', 'Rua dos Guararapes, 745, São Paulo-SP');
-INSERT INTO Usuario (id, nome, tipo, email, senha, telefone)
-	VALUES (2, 'Eric Castro', 'funcionário', 'ericc14@gmail.com', '12345678', '(11) 95555-9999');
-INSERT INTO Usuario 
-	VALUES (3, 'Gabriel', 'cliente', 'Gabriel2000@gmail.com', 'ABCD123', '(11) 95555-1001', 'Estrada de taipas, 1990, São Paulo-SP');
-INSERT INTO Usuario 
-	VALUES (4, 'Geovanna Vieira', 'cliente', 'Geovieira059@gmail.com', 'ABCD123', '(11) 95555-1002', 'Estrada de taipas, 1990, São Paulo-SP');
-INSERT INTO Usuario 
-	VALUES (5, 'Rosangela Santos', 'cliente', 'Rosangela.santos@gmail.com', '0246810', '(11) 95555-1030', 'Estrada de taipas, 1990, São Paulo-SP');
-SELECT * FROM Usuario;
-
-
--- INSERÇÃO DOS LIVROS
-INSERT INTO Livro VALUES (1, 'Memórias Póstumas', 'Machado De Assis', 'Antofágica', 200, 2, 'PT/BR', 2007, 'novo', 8, 2, 20, 2);
-INSERT INTO Livro VALUES (2, 'Mentirosos', 'Lockhart', 'Seguinte', 420, 1, 'PT/BR', 2016, 'novo', 7, 7, 50, 4);
-INSERT INTO Livro VALUES (3, 'Memórias Póstumas', 'Machado De Assis', 'Antofágica', 200, 2, 'PT/BR', 2007, 'usado', 8, 2, 20, 2);
-INSERT INTO Livro VALUES (4, 'Mentirosos', 'Lockhart', 'Seguinte', 420, 1, 'PT/BR', 2016, 'usado', 5, 0, 20, 2);
-INSERT INTO Livro VALUES (5, 'Luar vermelho', 'Gabriel', 'Companhia das letras', 226, 2, 'PT/BR', 2014, 'novo', 17, 3, 39, 5);
-INSERT INTO Livro VALUES (6, 'Harry Potter 1', 'JK.', 'Rocco', 226, 2, 'PT/BR', 2019, 'usado', 9, 5, 27, 4);
-INSERT INTO Livro VALUES (7, 'Harry Potter 2', 'JK.', 'Rocco', 423, 1, 'PT/BR', 2012, 'usado', 11, 2, 22, 3);
-SELECT * FROM Livro ORDER BY id ASC;
