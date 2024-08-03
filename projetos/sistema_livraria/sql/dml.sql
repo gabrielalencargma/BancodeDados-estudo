de345f19b5a35ad9fcde4fbacdc86ebe7a5300a0
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-08-03 02:12:39 -0300

    Update dml.sql

--- projetos/sistema_livraria/sql/dml.sql
+++ projetos/sistema_livraria/sql/dml.sql
@@ -33,237 +33,236 @@ SELECT * FROM Livro ORDER BY id ASC;
 */
 
 -- Carrinho do pedido 98
-insert into Pedido (id, id_usuario, status) values (98, 3, 'Aguardando');
-insert into Carrinho values (98, 7, 1, 22, 2);
+INSERT INTO Pedido (id, id_usuario, status) VALUES (98, 3, 'Aguardando');
+INSERT INTO Carrinho VALUES (98, 7, 1, 22, 2);
 
 -- Carrinho do pedido 99
-insert into Pedido (id, id_usuario, status) values (99, 4, 'Aguardando');
-insert into Carrinho values (99, 6, 1, 27, 4);
+INSERT INTO Pedido (id, id_usuario, status) VALUES (99, 4, 'Aguardando');
+INSERT INTO Carrinho VALUES (99, 6, 1, 27, 4);
 
 -- Atualizando tipo do pedido 99 para empréstimo
-update Pedido set tipo='Empréstimo' where id=99;
+UPDATE Pedido SET tipo='Empréstimo' WHERE id=99;
 
 -------------------------- PEDIDO DO TIPO COMPRA ------------------------------
 
 -- PARTE 1: CRIAÇÃO DE PEDIDO
-insert into Pedido (id, id_usuario, status) values (100, 1, 'Aguardando');
-insert into Carrinho values (100, 5, 1, 39, 5);
-insert into Carrinho values (100, 2, 2, 100, 2);
+INSERT INTO Pedido (id, id_usuario, status) VALUES (100, 1, 'Aguardando');
+INSERT INTO Carrinho VALUES (100, 5, 1, 39, 5);
+INSERT INTO Carrinho VALUES (100, 2, 2, 100, 2);
 
 -- PARTE 2: CONFIRMAÇÃO DE PEDIDO
 -- MÉTODO DE AQUISIÇÃO 2.1: 
-update Pedido set tipo='Compra' where id=100;
+UPDATE Pedido SET tipo='Compra' WHERE id=100;
 -- PAGAMENTO 2.2:
-update Pedido set forma_pagamento='Cartão', valor_total = 270 where id = 100;
+UPDATE Pedido SET forma_pagamento='Cartão', valor_total = 270 WHERE id = 100;
 -- CONFIRMAÇÃO 2.3:
-update Pedido set status='Aprovado', data_ped=TO_DATE('06/11/2023', 'DD/MM/YYYY'),
-	where id = 100;
+UPDATE Pedido SET status='Aprovado', data_ped=TO_DATE('06/11/2023', 'DD/MM/YYYY'),
+	WHERE id = 100;
 
 -- PARTE 3: FINALIZAÇÃO
-update Pedido set data_fim=TO_DATE('12/11/2023', 'DD/MM/YYYY') where id=100; 
+UPDATE Pedido SET data_fim=TO_DATE('12/11/2023', 'DD/MM/YYYY') WHERE id=100; 
 
 -------------------- PEDIDO DO TIPO COMPRA COM PERMUTA --------------------------
 
 -- PARTE 1: CRIAÇÃO DE PEDIDO
-insert into Pedido (id, id_usuario, status) values (101, 1, 'Aguardando'); 
-insert into Carrinho values (101, 5, 1, 39, 2);
+INSERT INTO Pedido (id, id_usuario, status) VALUES (101, 1, 'Aguardando'); 
+INSERT INTO Carrinho VALUES (101, 5, 1, 39, 2);
 
 -- PARTE 2: CONFIRMAÇÃO DE PEDIDO
 -- MÉTODO DE AQUISIÇÃO 2.1: 
-update Pedido set tipo='Compra' where id=101;
+UPDATE Pedido SET tipo='Compra' WHERE id=101;
 -- PAGAMENTO 2.2:
-update Pedido set forma_pagamento='Permuta' where id = 101;
+UPDATE Pedido SET forma_pagamento='Permuta' WHERE id = 101;
 -- CONFIRMAÇÃO 2.3:
-update Pedido set
+UPDATE Pedido SET
     status='Aprovado', data_ped=TO_DATE('17/11/2023', 'DD/MM/YYYY')
-    where id = 101;
+    WHERE id = 101;
 
 -- PARTE 3: FINALIZAÇÃO
-update Pedido set data_fim=TO_DATE('22/11/2023', 'DD/MM/YYYY') where id=101;
+UPDATE Pedido SET data_fim=TO_DATE('22/11/2023', 'DD/MM/YYYY') WHERE id=101;
 
 ---------------------- PEDIDO DO TIPO EMPRÉSTIMO ----------------------
 
 -- PARTE 1: CRIAÇÃO DE PEDIDO
-insert into Pedido (id, id_usuario, status) values (102, 3, 'Aguardando');
-insert into Carrinho values (102, 3, 1, 15, 2); 
-insert into Carrinho values (102, 4, 1, 25, 2);
+INSERT INTO Pedido (id, id_usuario, status) VALUES (102, 3, 'Aguardando');
+INSERT INTO Carrinho VALUES (102, 3, 1, 15, 2); 
+INSERT INTO Carrinho VALUES (102, 4, 1, 25, 2);
 
 -- PARTE 2: CONFIRMAÇÃO DE PEDIDO
 -- PARTE 2.1: MÉTODO DE AQUISIÇÃO:
-update Pedido set tipo='Empréstimo' where id=102;
+UPDATE Pedido SET tipo='Empréstimo' WHERE id=102;
 -- CONFIRMAÇÃO 2.2:
-update Pedido set 
-    status='Aprovado', data_ped=TO_DATE('17/11/2023', 'DD/MM/YYYY') where id = 102;
+UPDATE Pedido SET 
+    status='Aprovado', data_ped=TO_DATE('17/11/2023', 'DD/MM/YYYY') WHERE id = 102;
 
 -- PARTE 3: FINALIZAÇÃO
 -- Emprestado (entregue)
-update Pedido set status='Emprestado', data_emp=TO_DATE('24/11/2023', 'DD/MM/YYYY'), data_fim=TO_DATE('02/01/2024', 'DD/MM/YYYY') 
-    where id = 102;
+UPDATE Pedido SET status='Emprestado', data_emp=TO_DATE('24/11/2023', 'DD/MM/YYYY'), data_fim=TO_DATE('02/01/2024', 'DD/MM/YYYY') 
+    WHERE id = 102;
 -- Devolvido
-update Pedido set
-    status='Devolvido', data_fim=TO_DATE('16/12/2024', 'DD/MM/YYYY') where id = 102;
+UPDATE Pedido SET
+    status='Devolvido', data_fim=TO_DATE('16/12/2024', 'DD/MM/YYYY') WHERE id = 102;
 
 ----------------------- REEMBOLSO --------------------------
 
 -- PARTE 1: CRIAÇÃO DE PEDIDO
-insert into Pedido (id, id_usuario, status) values (103, 5, 'Aguardando');
-insert into Carrinho values (103, 6, 1, 27, 4); 
-insert into Carrinho values (103, 7, 1, 22, 3);
+INSERT INTO Pedido (id, id_usuario, status) VALUES (103, 5, 'Aguardando');
+INSERT INTO Carrinho VALUES (103, 6, 1, 27, 4); 
+INSERT INTO Carrinho VALUES (103, 7, 1, 22, 3);
 
 -- PARTE 2: CONFIRMAÇÃO DE PEDIDO
 -- MÉTODO DE AQUISIÇÃO 2.1: 
-update Pedido set tipo='Compra' where id=103;
+UPDATE Pedido SET tipo='Compra' WHERE id=103;
 -- PAGAMENTO 2.2:
-update Pedido set forma_pagamento='Pix', valor_total = 58 where id = 103;
+UPDATE Pedido SET forma_pagamento='Pix', valor_total = 58 WHERE id = 103;
 -- PAGAMENTO 2.2:
-update Pedido set
+UPDATE Pedido SET
     status='Aprovado', data_ped=TO_DATE('10/10/2023', 'DD/MM/YYYY')
-    where id = 103;
+    WHERE id = 103;
 
 -- PARTE 3: FINALIZAÇÃO
-update Pedido set data_fim=TO_DATE('20/10/2023', 'DD/MM/YYYY') where id=103;
+UPDATE Pedido SET data_fim=TO_DATE('20/10/2023', 'DD/MM/YYYY') WHERE id=103;
 
 -- REEMBOLSO
-update Pedido set status='Em reembolso', motivo_reemb='Tamanho do produto',
+UPDATE Pedido SET status='Em reembolso', motivo_reemb='Tamanho do produto',
 
 -- PARTE 1: CRIAÇÃO DO PEDIDO
-insert into Pedido (id, id_usuario, status) values (104, 4, 'Aguardando');
-insert into Carrinho values (104, 1, 2, 40, 5); 
-insert into Carrinho values (104, 5, 1, 39, 5);
+INSERT INTO Pedido (id, id_usuario, status) VALUES (104, 4, 'Aguardando');
+INSERT INTO Carrinho VALUES (104, 1, 2, 40, 5); 
+INSERT INTO Carrinho VALUES (104, 5, 1, 39, 5);
 
 -- PARTE 2: CONFIRMAÇÃO DE PEDIDO
 -- MÉTODO DE AQUISIÇÃO 2.1:
-update Pedido set tipo='Compra' where id=104;
+UPDATE Pedido SET tipo='Compra' WHERE id=104;
 -- PAGAMENTO 2.2:
-update Pedido set forma_pagamento='Pix', valor_total = 79 where id = 104;
+UPDATE Pedido SET forma_pagamento='Pix', valor_total = 79 WHERE id = 104;
 -- CONFIRMAÇÃO 2.3:
-update Pedido set status='Aprovado', data_ped=TO_DATE('15/10/2023', 'DD/MM/YYYY')
-	where id = 104;
+UPDATE Pedido SET status='Aprovado', data_ped=TO_DATE('15/10/2023', 'DD/MM/YYYY')
+	WHERE id = 104;
 
 -- PARTE 3: FINALIZAÇÃO
-update Pedido set data_fim=TO_DATE('23/10/2023', 'DD/MM/YYYY') where id=104;
+UPDATE Pedido SET data_fim=TO_DATE('23/10/2023', 'DD/MM/YYYY') WHERE id=104;
 
 -- Reembolso
-update Pedido set status='Em reembolso', motivo_reemb='Produto quebrado' where id=104;
+UPDATE Pedido SET status='Em reembolso', motivo_reemb='Produto quebrado' WHERE id=104;
 -- Reembolso finalizado
-update Pedido set status='Reembolsado', data_reemb=TO_DATE('02/11/2023', 'DD/MM/YYYY') where id=104;
+UPDATE Pedido SET status='Reembolsado', data_reemb=TO_DATE('02/11/2023', 'DD/MM/YYYY') WHERE id=104;
 
 
 
 /* SELECTS */
 -- TODOS PEDIDOS
-select * from Pedido order by id asc;
+SELECT * FROM Pedido ORDER BY id ASC;
 
 -- TODOS CARRINHOS
-select * from Carrinho order by id_pedido asc;
+SELECT * FROM Carrinho ORDER BY id_pedido ASC;
 
 -- TODOS LIVRO
-select * from Livro where condicao = 'usado';
+SELECT * FROM Livro WHERE condicao = 'usado';
 
 -- CADASTRO USUARIO
-insert into Usuario values (1, 'Eric Castro', 'cliente', 'ericc14@gmail.com', '12345678', '(11) 95555-9999', 'Rua dos Guararapes, 745, São Paulo-SP');
+INSERT INTO Usuario VALUES (1, 'Eric Castro', 'cliente', 'ericc14@gmail.com', '12345678', '(11) 95555-9999', 'Rua dos Guararapes, 745, São Paulo-SP');
 
 -- FUNÇÕES VITRINE
-select titulo, autor, editora, paginas, volume, idioma, ano_publicacao, condicao, valor_unitario, pontos_troca from Livro where id = 1;
+SELECT titulo, autor, editora, paginas, volume, idioma, ano_publicacao, condicao, valor_unitario, pontos_troca FROM Livro WHERE id = 1;
 
-insert into Pedido (id, id_usuario, tipo, status) values (105, 1, 'Compra', 'Aguardando'); 
-insert into Carrinho values (105, 5, 1, 39, 2);
+INSERT INTO Pedido (id, id_usuario, tipo, status) VALUES (105, 1, 'Compra', 'Aguardando'); 
+INSERT INTO Carrinho VALUES (105, 5, 1, 39, 2);
 
-insert into Pedido (id, id_usuario, status) values (98, 3, 'Aguardando');
-insert into Carrinho values (98, 7, 1, 22, 2);
+INSERT INTO Pedido (id, id_usuario, status) VALUES (98, 3, 'Aguardando');
+INSERT INTO Carrinho VALUES (98, 7, 1, 22, 2);
 
-insert into Pedido (id, id_usuario, status) values (100, 1, 'Aguardando');
-insert into Carrinho values (100, 5, 1, 39, 5);
-insert into Carrinho values (100, 2, 2, 100, 2);
-update Pedido set tipo='Compra' where id=100;
+INSERT INTO Pedido (id, id_usuario, status) VALUES (100, 1, 'Aguardando');
+INSERT INTO Carrinho VALUES (100, 5, 1, 39, 5);
+INSERT INTO Carrinho VALUES (100, 2, 2, 100, 2);
+UPDATE Pedido SET tipo='Compra' WHERE id=100;
 
-insert into Pedido (id, id_usuario, tipo, status) values (106, 3, 'Empréstimo', 'Aguardando');
-insert into Carrinho values (106, 3, 1, 15, 2); 
+INSERT INTO Pedido (id, id_usuario, tipo, status) VALUES (106, 3, 'Empréstimo', 'Aguardando');
+INSERT INTO Carrinho VALUES (106, 3, 1, 15, 2); 
 
 
 -- FUNÇÕES DO CARRINHO
-select titulo, valor_unitario, pontos_troca from Livro where id=3 or id=4;
+SELECT titulo, valor_unitario, pontos_troca FROM Livro WHERE id=3 or id=4;
 
-update Carrinho set quantidade = 3, infratotal = 66, pontos_totais = 9 where id_pedido = 98 and id_livro = 7;
+UPDATE Carrinho SET quantidade = 3, infratotal = 66, pontos_totais = 9 WHERE id_pedido = 98 AND id_livro = 7;
 
-delete from Carrinho where id_pedido = 98 and id_livro = 7;
+delete FROM Carrinho WHERE id_pedido = 98 AND id_livro = 7;
 
-select id_pedido, SUM(pontosTotais) "Subtotal troca", SUM(infratotal) "Subtotal preço" from Carrinho where id_pedido = 102 
-    group by id_pedido order by id_pedido asc;
+SELECT id_pedido, SUM(pontosTotais) "Subtotal troca", SUM(infratotal) "Subtotal preço" FROM Carrinho WHERE id_pedido = 102 
+    GROUP BY id_pedido ORDER BY id_pedido ASC;
 
-update Carrinho set quantidade = 3, infratotal = 66 where id_pedido = 98 and id_livro = 7;
+UPDATE Carrinho SET quantidade = 3, infratotal = 66 WHERE id_pedido = 98 AND id_livro = 7;
 
-update Pedido set tipo='Empréstimo' where id=99;
+UPDATE Pedido SET tipo='Empréstimo' WHERE id=99;
 
-select id_pedido, SUM(quantidade) "Total de Livros" from Carrinho where id_pedido = 102 group by id_pedido order by id_pedido asc;
+SELECT id_pedido, SUM(quantidade) "Total de Livros" FROM Carrinho WHERE id_pedido = 102 GROUP BY id_pedido ORDER BY id_pedido ASC;
 
 -- CONFIRMAÇÃO PEDIDO (COMPRA, EMPRÉSTIMO, PERMUTA)
-select us.nome, us.endereco, l.titulo, l.condicao, c.quantidade from Usuario us 
-    inner join Pedido p ON us.id=p.id_usuario
-    inner join Carrinho c ON p.id=c.id_pedido
-    inner join Livro l ON c.id_livro=l.id
-    where us.id = 4 and p.id = 104;
+SELECT us.nome, us.endereco, l.titulo, l.condicao, c.quantidade FROM Usuario us 
+    INNER JOIN Pedido p ON us.id=p.id_usuario
+    INNER JOIN Carrinho c ON p.id=c.id_pedido
+    INNER JOIN Livro l ON c.id_livro=l.id
+    WHERE us.id = 4 AND p.id = 104;
 
-select us.nome, us.endereco, l.titulo, l.condicao, c.pontos_totais from Usuario us 
-    inner join Pedido p ON us.id=p.id_usuario
-    inner join Carrinho c ON p.id=c.id_pedido
-    inner join Livro l ON c.id_livro=l.id
-    where us.id = 1 and p.id = 101;
+SELECT us.nome, us.endereco, l.titulo, l.condicao, c.pontos_totais FROM Usuario us 
+    INNER JOIN Pedido p ON us.id=p.id_usuario
+    INNER JOIN Carrinho c ON p.id=c.id_pedido
+    INNER JOIN Livro l ON c.id_livro=l.id
+    WHERE us.id = 1 AND p.id = 101;
 
-update Pedido set status='Aprovado', data_ped=TO_DATE('17/11/2023', 'DD/MM/YYYY') where id = 101;
+UPDATE Pedido SET status='Aprovado', data_ped=TO_DATE('17/11/2023', 'DD/MM/YYYY') WHERE id = 101;
 
-select valor_total from Pedido where id = 100;
+SELECT valor_total FROM Pedido WHERE id = 100;
 
-select SUM(pontos_totais) "Subtotal troca" from Carrinho where id_pedido = 101 
-    group by id_pedido order by id_pedido asc;
+SELECT SUM(pontos_totais) "Subtotal troca" FROM Carrinho WHERE id_pedido = 101 
+    GROUP BY id_pedido ORDER BY id_pedido ASC;
 
 -- HISTÓRICO PEDIDOS
-select p.id, p.status, p.data_ped
+SELECT p.id, p.status, p.data_ped
 
 -- PAGAMENTO
-update Pedido set forma_pagamento='Cartão', valor_total=270 where id = 100;
+UPDATE Pedido SET forma_pagamento='Cartão', valor_total=270 WHERE id = 100;
 
 -- REEMBOLSO (SOLICITAR)
-update Pedido set status='Em reembolso', motivo_reemb='Produto quebrado' where id=104;
+UPDATE Pedido SET status='Em reembolso', motivo_reemb='Produto quebrado' WHERE id=104;
 
 -- CADASTRO FUNCIONÁRIOS
-insert into Usuario (id, nome, tipo, email, senha, telefone)
-    values (2, 'Eric Castro', 'funcionário', 'ericc14@gmail.com', '12345678', '(11) 95555-9999');
+INSERT INTO Usuario (id, nome, tipo, email, senha, telefone)
+    VALUES (2, 'Eric Castro', 'funcionário', 'ericc14@gmail.com', '12345678', '(11) 95555-9999');
 
 -- LIVROS CADASTRADOS (VER)
-select id, titulo, qtd_disponivel, qtd_reservado from Livro;
-select titulo, qtd_disponivel, qtd_reservado from Livro where id = 2;
+SELECT id, titulo, qtd_disponivel, qtd_reservado FROM Livro;
+SELECT titulo, qtd_disponivel, qtd_reservado FROM Livro WHERE id = 2;
 
 -- CADASTRAR LIVRO
-insert into Livro values (1, 'Memórias Póstumas', 'Machado De Assis', 'Antofágica', 200, 2, 'PT/BR', 2007, 'novo', 8, 2, 20, 2);
-
+INSERT INTO Livro VALUES (1, 'Memórias Póstumas', 'Machado De Assis', 'Antofágica', 200, 2, 'PT/BR', 2007, 'novo', 8, 2, 20, 2);
 
 -- LIVROS EM CADA PEDIDO
-select p.id, p.tipo, p.status, us.nome, c.id_livro, l.titulo, l.condicao, p.tipo from Pedido p
-    inner join Carrinho c ON p.id=c.id_pedido
-    inner join Livro l ON l.id=c.id_livro 
-    inner join Usuario us ON us.id=p.id_usuario
-    where us.id=4 order by id asc
+SELECT p.id, p.tipo, p.status, us.nome, c.id_livro, l.titulo, l.condicao, p.tipo FROM Pedido p
+    INNER JOIN Carrinho c ON p.id=c.id_pedido
+    INNER JOIN Livro l ON l.id=c.id_livro 
+    INNER JOIN Usuario us ON us.id=p.id_usuario
+    WHERE us.id=4 ORDER BY id ASC;
     
 -- LIVROS EM CADA PEDIDO DE DETERMINADO USUARIO
-select p.id "Pedido", p.tipo, p.status, p.id_usuario, us.nome, c.id_livro, l.titulo, l.condicao, p.tipo from Pedido p
-    inner join Carrinho c ON p.id=c.id_pedido
-    inner join Livro l ON l.id=c.id_livro
-    inner join Usuario us ON us.id=p.id_usuario
-    where p.id_usuario = 1;
+SELECT p.id "Pedido", p.tipo, p.status, p.id_usuario, us.nome, c.id_livro, l.titulo, l.condicao, p.tipo FROM Pedido p
+    INNER JOIN Carrinho c ON p.id=c.id_pedido
+    INNER JOIN Livro l ON l.id=c.id_livro
+    INNER JOIN Usuario us ON us.id=p.id_usuario
+    WHERE p.id_usuario = 1;
 
 -- LIVROS DE UM DETERMINADO PEDIDO DE UM DETERMINADO USUARIO
-select p.id "Pedido", p. tipo, p.status, p.id_usuario, us.nome, c.id_livro, l.titulo, l.condicao, p.tipo from Pedido p
-    inner join Carrinho c ON p.id=c.id_pedido
-    inner join Livro l ON l.id=c.id_livro
-    inner join Usuario us ON us.id=p.id_usuario
-    where p.id_usuario = 1 and p.id=100;
+SELECT p.id "Pedido", p. tipo, p.status, p.id_usuario, us.nome, c.id_livro, l.titulo, l.condicao, p.tipo FROM Pedido p
+    INNER JOIN Carrinho c ON p.id=c.id_pedido
+    INNER JOIN Livro l ON l.id=c.id_livro
+    INNER JOIN Usuario us ON us.id=p.id_usuario
+    WHERE p.id_usuario = 1 AND p.id=100;
 
 
 -- SUBTOTAL E SEUS RESPECTIVOS USUARIOS
-select c.id_pedido, p.id_usuario, SUM(c.infratotal) "Subtotal" from Carrinho c 
-    inner join Pedido p ON p.id=c.id_pedido
-	group by c.id_pedido, p.id_usuario order by c.id_pedido asc;
+SELECT c.id_pedido, p.id_usuario, SUM(c.infratotal) "Subtotal" FROM Carrinho c 
+    INNER JOIN Pedido p ON p.id=c.id_pedido
+	GROUP BY c.id_pedido, p.id_usuario ORDER BY c.id_pedido ASC;
 
 -- TODAS COMPRAS
-select * from Pedido where tipo = 'Compra';
+SELECT * FROM Pedido WHERE tipo = 'Compra';
