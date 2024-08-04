9f9b4309461964222e455d8d6eb64bede562e952
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-08-03 23:57:57 -0300

    Organizando o código
    Estou separando em dois arquivos sql e passando o que está no dml.sql para o teste.sql. Desta forma, estarei revisando e organizando o código, além de atualizar os comentários.

--- projetos/sistema_livraria/sql/dml.sql
+++ projetos/sistema_livraria/sql/dml.sql
@@ -1,14 +1,3 @@
------ CADASTRO -----
-
--- Cadastro de clientes
-INSERT INTO Usuario 
-	VALUES (1, 'Eric Castro', 'cliente', 'ericc14@gmail.com', '12345678', '(11) 95555-9999', 'Rua dos Guararapes, 745, São Paulo-SP');
-INSERT INTO Usuario 
-	VALUES (3, 'Gabriel', 'cliente', 'Gabriel2000@gmail.com', 'ABCD123', '(11) 95555-1001', 'Estrada de taipas, 1990, São Paulo-SP');
-INSERT INTO Usuario 
-	VALUES (4, 'Geovanna Vieira', 'cliente', 'Geovieira059@gmail.com', 'ABCD123', '(11) 95555-1002', 'Estrada de taipas, 1990, São Paulo-SP');
-INSERT INTO Usuario 
-	VALUES (5, 'Rosangela Santos', 'cliente', 'Rosangela.santos@gmail.com', '0246810', '(11) 95555-1030', 'Estrada de taipas, 1990, São Paulo-SP');
 
 -- Cadastro de Funcionários
 INSERT INTO Usuario (id, nome, tipo, email, senha, telefone)
@@ -23,63 +12,7 @@ INSERT INTO Livro VALUES (5, 'Luar vermelho', 'Gabriel', 'Companhia das letras',
 INSERT INTO Livro VALUES (6, 'Harry Potter 1', 'JK.', 'Rocco', 226, 2, 'PT/BR', 2019, 'usado', 9, 5, 27, 4);
 INSERT INTO Livro VALUES (7, 'Harry Potter 2', 'JK.', 'Rocco', 423, 1, 'PT/BR', 2012, 'usado', 11, 2, 22, 3);
 SELECT * FROM Livro ORDER BY id ASC;
-
------ CRIAÇÃO DO CARRINHO DE COMPRAS -----
-
-/* Após clicar em adicionar ao carrinho: 
-	1-será aberto um pedido
-	2-será feita, no carrinho, a escolha de como deseja adquirir este produto (compra ou empréstimo)
-	3-processos posteriores em função do método de aquisição selecionado
-*/
-
--- Carrinho do pedido 98
-INSERT INTO Pedido (id, id_usuario, status) VALUES (98, 3, 'Aguardando');
-INSERT INTO Carrinho VALUES (98, 7, 1, 22, 2);
-
--- Carrinho do pedido 99
-INSERT INTO Pedido (id, id_usuario, status) VALUES (99, 4, 'Aguardando');
-INSERT INTO Carrinho VALUES (99, 6, 1, 27, 4);
-
--- Atualizando tipo do pedido 99 para empréstimo
-UPDATE Pedido SET tipo='Empréstimo' WHERE id=99;
-
--------------------------- PEDIDO DO TIPO COMPRA ------------------------------
-
--- PARTE 1: CRIAÇÃO DE PEDIDO
-INSERT INTO Pedido (id, id_usuario, status) VALUES (100, 1, 'Aguardando');
-INSERT INTO Carrinho VALUES (100, 5, 1, 39, 5);
-INSERT INTO Carrinho VALUES (100, 2, 2, 100, 2);
-
--- PARTE 2: CONFIRMAÇÃO DE PEDIDO
--- MÉTODO DE AQUISIÇÃO 2.1: 
-UPDATE Pedido SET tipo='Compra' WHERE id=100;
--- PAGAMENTO 2.2:
-UPDATE Pedido SET forma_pagamento='Cartão', valor_total = 270 WHERE id = 100;
--- CONFIRMAÇÃO 2.3:
-UPDATE Pedido SET status='Aprovado', data_ped=TO_DATE('06/11/2023', 'DD/MM/YYYY'),
-	WHERE id = 100;
-
--- PARTE 3: FINALIZAÇÃO
-UPDATE Pedido SET data_fim=TO_DATE('12/11/2023', 'DD/MM/YYYY') WHERE id=100; 
-
--------------------- PEDIDO DO TIPO COMPRA COM PERMUTA --------------------------
-
--- PARTE 1: CRIAÇÃO DE PEDIDO
-INSERT INTO Pedido (id, id_usuario, status) VALUES (101, 1, 'Aguardando'); 
-INSERT INTO Carrinho VALUES (101, 5, 1, 39, 2);
-
--- PARTE 2: CONFIRMAÇÃO DE PEDIDO
--- MÉTODO DE AQUISIÇÃO 2.1: 
-UPDATE Pedido SET tipo='Compra' WHERE id=101;
--- PAGAMENTO 2.2:
-UPDATE Pedido SET forma_pagamento='Permuta' WHERE id = 101;
--- CONFIRMAÇÃO 2.3:
-UPDATE Pedido SET
-    status='Aprovado', data_ped=TO_DATE('17/11/2023', 'DD/MM/YYYY')
-    WHERE id = 101;
-
--- PARTE 3: FINALIZAÇÃO
-UPDATE Pedido SET data_fim=TO_DATE('22/11/2023', 'DD/MM/YYYY') WHERE id=101;
+------------------------------------------------------------------------
 
 ---------------------- PEDIDO DO TIPO EMPRÉSTIMO ----------------------
 
@@ -162,40 +95,6 @@ SELECT * FROM Livro WHERE condicao = 'usado';
 
 -- CADASTRO USUARIO
 INSERT INTO Usuario VALUES (1, 'Eric Castro', 'cliente', 'ericc14@gmail.com', '12345678', '(11) 95555-9999', 'Rua dos Guararapes, 745, São Paulo-SP');
-
--- FUNÇÕES VITRINE
-SELECT titulo, autor, editora, paginas, volume, idioma, ano_publicacao, condicao, valor_unitario, pontos_troca FROM Livro WHERE id = 1;
-
-INSERT INTO Pedido (id, id_usuario, tipo, status) VALUES (105, 1, 'Compra', 'Aguardando'); 
-INSERT INTO Carrinho VALUES (105, 5, 1, 39, 2);
-
-INSERT INTO Pedido (id, id_usuario, status) VALUES (98, 3, 'Aguardando');
-INSERT INTO Carrinho VALUES (98, 7, 1, 22, 2);
-
-INSERT INTO Pedido (id, id_usuario, status) VALUES (100, 1, 'Aguardando');
-INSERT INTO Carrinho VALUES (100, 5, 1, 39, 5);
-INSERT INTO Carrinho VALUES (100, 2, 2, 100, 2);
-UPDATE Pedido SET tipo='Compra' WHERE id=100;
-
-INSERT INTO Pedido (id, id_usuario, tipo, status) VALUES (106, 3, 'Empréstimo', 'Aguardando');
-INSERT INTO Carrinho VALUES (106, 3, 1, 15, 2); 
-
-
--- FUNÇÕES DO CARRINHO
-SELECT titulo, valor_unitario, pontos_troca FROM Livro WHERE id=3 or id=4;
-
-UPDATE Carrinho SET quantidade = 3, infratotal = 66, pontos_totais = 9 WHERE id_pedido = 98 AND id_livro = 7;
-
-delete FROM Carrinho WHERE id_pedido = 98 AND id_livro = 7;
-
-SELECT id_pedido, SUM(pontosTotais) "Subtotal troca", SUM(infratotal) "Subtotal preço" FROM Carrinho WHERE id_pedido = 102 
-    GROUP BY id_pedido ORDER BY id_pedido ASC;
-
-UPDATE Carrinho SET quantidade = 3, infratotal = 66 WHERE id_pedido = 98 AND id_livro = 7;
-
-UPDATE Pedido SET tipo='Empréstimo' WHERE id=99;
-
-SELECT id_pedido, SUM(quantidade) "Total de Livros" FROM Carrinho WHERE id_pedido = 102 GROUP BY id_pedido ORDER BY id_pedido ASC;
 
 -- CONFIRMAÇÃO PEDIDO (COMPRA, EMPRÉSTIMO, PERMUTA)
 SELECT us.nome, us.endereco, l.titulo, l.condicao, c.quantidade FROM Usuario us 
